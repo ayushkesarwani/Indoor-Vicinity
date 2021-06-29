fbaaa0de66dd29d977b96958065f8f89f14b77e6
ayushkesarwani786 <ayushkesarwani786@gmail.com>
2021-06-29 16:37:28 +0530

    added animation

--- IndoorVicinity/app/src/main/java/com/tcs/indoorvicinity/MainActivity.java
+++ IndoorVicinity/app/src/main/java/com/tcs/indoorvicinity/MainActivity.java
@@ -9,7 +9,9 @@ import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +24,60 @@ public class MainActivity extends AppCompatActivity {
     static final int MIN_DISTANCE = 150;
     TextView t1,t2,vendor_text,cust_text;
     Animation topanim,leftanim,rightanim;
+    LinearLayout leftView,rightView;
+
+
+
+    // slide the view from below itself to the current position
+    public void slideLeft(View view){
+        view.setVisibility(View.VISIBLE);
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                -view.getWidth(),                 // toXDelta
+                0,  // fromYDelta
+                0);                // toYDelta
+        animate.setDuration(2000);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+
+
+
+
+
+    }
+
+    public void slideRight(View view){
+        view.setVisibility(View.VISIBLE);
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                view.getWidth(),                 // toXDelta
+                0,  // fromYDelta
+                0);                // toYDelta
+        animate.setDuration(2000);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+    }
+
+
+    public void centralise(View view){
+        view.setVisibility(View.VISIBLE);
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                0,  // fromYDelta
+                0);                // toYDelta
+        animate.setDuration(2000);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        centralise(leftView);
+        centralise(rightView);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,19 +89,32 @@ public class MainActivity extends AppCompatActivity {
         vendor_text=findViewById(R.id.vendor_text);
         cust_text=findViewById(R.id.cust_text);
 
+
+        leftView = findViewById(R.id.leftView);
+        rightView = findViewById(R.id.rightView);
+
+        // initialize as invisible (could also do in xml)
+
+
+
+
+
         topanim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
         leftanim=AnimationUtils.loadAnimation(this,R.anim.left_animation);
         rightanim=AnimationUtils.loadAnimation(this,R.anim.right_animation);
 
         t1.setAnimation(topanim);
         t2.setAnimation(topanim);
-        vendor_text.setAnimation(leftanim);
-        cust_text.setAnimation(rightanim);
+//        vendor_text.setAnimation(leftanim);
+//        cust_text.setAnimation(rightanim);
     }
+
 
     @Override
     public boolean onTouchEvent(MotionEvent event)
     {
+        leftanim=AnimationUtils.loadAnimation(this,R.anim.left_animation);
+        vendor_text.setAnimation(leftanim);
         switch(event.getAction())
         {
             case MotionEvent.ACTION_DOWN:
@@ -61,18 +130,20 @@ public class MainActivity extends AppCompatActivity {
                     if (x2 > x1)
                     {
                         System.out.println("Left to right");
-//
-                        Intent i =new Intent(MainActivity.this,com.tcs.indoorvicinity.Login.class);
-                        startActivity(i);
+                        leftView.setVisibility(View.INVISIBLE);
+                        slideLeft(leftView);
+
+//                        Intent i =new Intent(MainActivity.this,com.tcs.indoorvicinity.Login.class);
+//                        startActivity(i);
 //                        leftanim=AnimationUtils.loadAnimation(this,R.anim.left_animation);
-//                        vendor_text.setAnimation(leftanim);
-//                        new Timer().schedule(new TimerTask() {
-//                            @Override
-//                            public void run() {
-//                                // this code will be executed after 2 seconds
-//                                callVendorPage();
-//                            }
-//                        }, 6000);
+
+                        new Timer().schedule(new TimerTask() {
+                            @Override
+                            public void run() {
+                                // this code will be executed after 2 seconds
+                                callVendorPage();
+                            }
+                        }, 2000);
 
                     }
 
@@ -80,13 +151,23 @@ public class MainActivity extends AppCompatActivity {
                     else
                     {
                         System.out.println("Right to left");
+                        rightView.setVisibility(View.INVISIBLE);
+                        slideRight(rightView);
 
 //                        rightanim=AnimationUtils.loadAnimation(this,R.anim.right_animation);
-//                        cust_text.setAnimation(rightanim);
-                        Intent i =new Intent(MainActivity.this,com.tcs.indoorvicinity.UserHome.class);
-                        startActivity(i);
+//
+                        new Timer().schedule(new TimerTask() {
+                            @Override
+                            public void run() {
+                                // this code will be executed after 2 seconds
+                                callCustomerPage();
+                            }
+                        }, 2000);
 
                     }
+
+
+
 
                 }
                 else
@@ -103,6 +184,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void callVendorPage(){
         Intent i =new Intent(MainActivity.this,com.tcs.indoorvicinity.Login.class);
+        startActivity(i);
+
+    }
+
+    public void callCustomerPage(){
+        Intent i =new Intent(MainActivity.this,com.tcs.indoorvicinity.UserHome.class);
         startActivity(i);
 
     }
